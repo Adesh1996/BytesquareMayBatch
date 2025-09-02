d0ac280d35ca732ba369987b30fb7f61ee85e298
Adesh1996 <adikhedkar1996@gmail.com>
2025-09-02 08:35:03 +0530

    Admin Page is created for Test

---
+++ src/main/java/Pages/AdminPage.java
@@ -0,0 +1,15 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class AdminPage {
+	
+public WebDriver driver;
+	
+	public AdminPage(WebDriver driver) {
+		this.driver= driver;
+		PageFactory.initElements(driver, this);
+	}
+
+}
